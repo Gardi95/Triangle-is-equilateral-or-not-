9551cd880ac9624f1c6a0eeb092b2bfc661dc592
Mohammed Yaba <91971316+Gardi95@users.noreply.github.com>
2022-06-29 22:40:54 +0200

    firstv commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+
+            Console.WriteLine("Enter the A Number");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the B Number ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the C Number");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            if (a + b + c == 180)
+            {
+                Console.WriteLine("Tha Triangle is Equilateral ");
+            }
+            else
+            {
+                Console.WriteLine("The Triangle is NOT Equilateral ");
+            }
+        }
+    }
+}
